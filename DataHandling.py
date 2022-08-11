9568c5565e1167d97023884d016aa3a42ddbba12
John Ervin <49054998+StJohnErvin@users.noreply.github.com>
2022-08-11 16:06:59 +0800

    Update DataHandling.py

--- DataHandling.py
+++ DataHandling.py
@@ -1,1 +1,29 @@
 #Data Handling
+
+import pandas as pd
+
+items = pd.Series(['chairs','labels', 'tables'])
+items
+
+items = pd.Series(['chairs','labels', 'tables'],index=['OR 001','OR 002','OR 003'])
+items
+
+names = pd.Series(['Joe','Tom', 'Jane'])
+names
+
+dframe = {'Name':['Joe','Tom','Jane'],
+          'Items':['Chair','labels','tables']
+         }
+purchases = pd.DataFrame(dframe)
+
+purchases
+
+mdata = pd.read_csv('Marketing.csv')
+
+mdata.head()
+mdata.head(10)
+mdata.tail()
+mdata.tail(2)
+mdata.info()
+mdata.head()
+mdata.shape
